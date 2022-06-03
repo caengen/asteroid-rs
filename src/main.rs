1b7910a07e9d40499f8b631935810a1cdf6bdd18
Christian Engen <chris.a.engen@gmail.com>
2022-06-03 15:10:24 +0200

    render testeroid

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,17 @@ const SCREEN_WIDTH: f32 = 600.0;
 const SCREEN_HEIGHT: f32 = 400.0;
 const ASTEROID_MAX_SIZE: i8 = 3;
 const ASTEROID_VELOCITY: f32 = 10.0;
+const FRICT: f32 = 0.75;
 // const UNITS: f32 = 32.0;
 const UNITS: f32 = 16.0;
 const PLAYER_WIDTH: f32 = 1.0;
 const PLAYER_HEIGHT: f32 = 1.0;
+const PLAYER_ACCL: f32 = 7.0;
+const ANGLE_STEP: f32 = 5.0;
 const BULLET_WIDTH: f32 = 4.0;
+const BULLET_VEL: f32 = 300.0;
 const BULLET_LIVE_TIME: f64 = 2.0;
 const TURRET_COOLDOWN: f64 = 0.5;
-const PLAYER_ACCELERATION: f32 = 10.0;
 
 enum RunState {
     Menu,
@@ -26,6 +29,7 @@ struct Asteroid {
     pos: Vec2,
     vel: Vec2,
     size: i8,
+    points: Vec<Vec2>,
 }
 
 struct Bullet {
@@ -40,7 +44,6 @@ struct Spaceship {
     pos: Vec2,
     angle: f32,
     vel: Vec2,
-    acc: f32,
     last_turret_frame: f64,
 }
 struct GameState {
@@ -60,22 +63,22 @@ fn handle_input(gs: &mut GameState) {
     match gs.run_state {
         RunState::Running => {
             if is_key_down(KeyCode::Left) {
-                gs.player.angle = -((gs.player.angle - 5.0).abs() % 360.0);
+                gs.player.angle = -((gs.player.angle - ANGLE_STEP).abs() % 360.0);
             }
             if is_key_down(KeyCode::Right) {
-                gs.player.angle = (gs.player.angle + 5.0) % 360.0;
+                gs.player.angle = (gs.player.angle + ANGLE_STEP) % 360.0;
             }
 
             if is_key_down(KeyCode::Up) {
                 gs.player.vel = vec2(
-                    gs.player.vel.x + (7.0 * delta) * rotation.sin(),
-                    gs.player.vel.y - (7.0 * delta) * rotation.cos(),
+                    gs.player.vel.x + (PLAYER_ACCL * delta) * rotation.sin(),
+                    gs.player.vel.y - (PLAYER_ACCL * delta) * rotation.cos(),
                 );
             }
             if is_key_down(KeyCode::Down) {
                 gs.player.vel = vec2(
-                    gs.player.vel.x - (7.0 * delta) * rotation.sin(),
-                    gs.player.vel.y + (7.0 * delta) * rotation.cos(),
+                    gs.player.vel.x - PLAYER_ACCL * delta * rotation.sin(),
+                    gs.player.vel.y + PLAYER_ACCL * delta * rotation.cos(),
                 );
             }
             let time = get_time();
@@ -83,11 +86,11 @@ fn handle_input(gs: &mut GameState) {
                 gs.player.last_turret_frame = time;
                 gs.bullets.push(Bullet {
                     pos: vec2(
-                        gs.player.pos.x + rotation.sin() * (sh / 2.),
-                        gs.player.pos.y - rotation.cos() * (sh / 2.),
+                        gs.player.pos.x + rotation.sin() * sh / 2.,
+                        gs.player.pos.y - rotation.cos() * sh / 2.,
                     ),
                     created_at: time,
-                    vel: gs.player.vel + vec2(200.0 * rotation.sin(), -(200.0 * rotation.cos())),
+                    vel: vec2(BULLET_VEL * rotation.sin(), -(BULLET_VEL * rotation.cos())),
                 })
             }
         }
@@ -112,24 +115,38 @@ fn wrap(pos: Vec2, width: f32, height: f32) -> Vec2 {
     new_pos
 }
 
+fn get_points(origo: Vec2, amount: i32, size: f32) -> Vec<Vec2> {
+    let mut points = Vec::new();
+    let angle_inc = 360.0 / amount as f32;
+    for i in 1..=amount {
+        let rot = (angle_inc * i as f32).to_radians();
+        let r = rand::gen_range(0.5, 1.0);
+        points.push(vec2(
+            origo.x + PLAYER_WIDTH * r * size * rot.sin(),
+            origo.y - PLAYER_WIDTH * r * size * rot.cos(),
+        ));
+    }
+
+    points
+}
+
 fn update(gs: &mut GameState) {
     let delta = get_frame_time();
     let time = get_time();
     match gs.run_state {
         RunState::Running => {
-            let rotation = gs.player.angle.to_radians();
             gs.player.pos = gs.player.pos + gs.player.vel;
             // apply space friction
             let mut new_vel = gs.player.vel;
             if gs.player.vel.x > 0.0 {
-                new_vel.x = clamp(gs.player.vel.x - (1.0 * delta), 0.0, gs.player.vel.x);
+                new_vel.x = clamp(gs.player.vel.x - (FRICT * delta), 0.0, gs.player.vel.x);
             } else {
-                new_vel.x = clamp(gs.player.vel.x + (1.0 * delta), gs.player.vel.x, 0.0);
+                new_vel.x = clamp(gs.player.vel.x + (FRICT * delta), gs.player.vel.x, 0.0);
             };
             if gs.player.vel.y > 0.0 {
-                new_vel.y = clamp(gs.player.vel.y - (1.0 * delta), 0.0, gs.player.vel.y);
+                new_vel.y = clamp(gs.player.vel.y - (FRICT * delta), 0.0, gs.player.vel.y);
             } else {
-                new_vel.y = clamp(gs.player.vel.y + (1.0 * delta), gs.player.vel.y, 0.0);
+                new_vel.y = clamp(gs.player.vel.y + (FRICT * delta), gs.player.vel.y, 0.0);
             };
             gs.player.vel = new_vel;
 
@@ -144,7 +161,7 @@ fn update(gs: &mut GameState) {
             gs.bullets
                 .retain(|b| time - b.created_at < BULLET_LIVE_TIME);
 
-            // handle bounds
+            // handle player bounds
             gs.player.pos = wrap(gs.player.pos, gs.player.w, gs.player.h)
         }
         _ => {}
@@ -180,9 +197,17 @@ fn draw_spaceship(ship: &Spaceship, scl: f32) {
 
     draw_triangle_lines(v1, v2, v3, 1.0, BLACK);
 
-    draw_circle(pos.x, pos.y, 0.1 * scl, RED);
+    // draw_circle(pos.x, pos.y, 0.1 * scl, RED);
 
-    draw_line(pos.x, pos.y, pos.x + vel.x, pos.y + vel.y, 1.0, GREEN)
+    // vel vector debugging
+    draw_line(
+        pos.x,
+        pos.y,
+        pos.x + vel.x * scl / 2.0,
+        pos.y + vel.y * scl / 2.0,
+        1.0,
+        GREEN,
+    );
 }
 
 fn draw(gs: &GameState) {
@@ -190,10 +215,18 @@ fn draw(gs: &GameState) {
 
     match gs.run_state {
         RunState::Running => {
-            draw_spaceship(&gs.player, gs.scl);
+            // draw_spaceship(&gs.player, gs.scl);
 
             for bullet in gs.bullets.iter() {
                 draw_circle(bullet.pos.x, bullet.pos.y, BULLET_WIDTH / 2.0, BLACK)
+            }
+
+            for asteroid in gs.asteroids.iter() {
+                for i in 0..=(asteroid.points.len() - 1) {
+                    let p1 = asteroid.points[i];
+                    let p2 = asteroid.points[(i + 1) % asteroid.points.len()];
+                    draw_line(p1.x, p1.y, p2.x, p2.y, 1.0, BLACK);
+                }
             }
 
             draw_text(
@@ -217,31 +250,37 @@ fn draw(gs: &GameState) {
 
 #[macroquad::main("Pong")]
 async fn main() {
+    request_new_screen_size(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+    let scale = screen_height() / UNITS;
     let mut gs = GameState {
         run_state: RunState::Running,
-        scl: screen_height() / UNITS,
+        scl: scale,
         player: Spaceship {
             w: PLAYER_WIDTH,
             h: PLAYER_HEIGHT,
             pos: vec2(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
             angle: 0.0,
             vel: vec2(0.0, 0.0),
-            acc: 0.0,
             last_turret_frame: 0.0,
         },
         asteroids: vec![
             Asteroid {
-                pos: vec2(0.0, 0.0),
-                vel: vec2(ASTEROID_VELOCITY, ASTEROID_VELOCITY),
-                size: 3
+                pos: vec2(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
+                // vel: vec2(ASTEROID_VELOCITY, ASTEROID_VELOCITY),
+                vel: vec2(0.0, 0.0),
+                size: 3,
+                points: get_points(
+                    vec2(SCREEN_WIDTH / 2.0, SCREEN_HEIGHT / 2.0),
+                    8,
+                    3.0 * scale / 2.0
+                )
             };
             6
         ],
         bullets: Vec::new(),
         lives: 5,
     };
-
-    request_new_screen_size(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     loop {
         gs.scl = screen_height() / UNITS;
